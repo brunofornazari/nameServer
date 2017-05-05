dc88be2769ea4420250ae7cc17268b7e77f8debe
brunofornazari <bruno_fornazari@hotmail.com>
2017-05-04 23:27:56 -0300

    Estrutura de Projeto

--- nameServer/src/BackEnd/ServidorImpl.java
+++ nameServer/src/BackEnd/ServidorImpl.java
@@ -14,21 +14,42 @@ public class ServidorImpl extends UnicastRemoteObject implements Servidor{
     @Override
     public ServidorIndividual getServidor(String sFuncao) {
         List<ServidorIndividual> servicos = _servicos;
+        System.out.println("Requisitando " + sFuncao);
         for(ServidorIndividual servico : servicos){
-            if(servico.getName() == sFuncao){
+            if(servico.getName().compareTo(sFuncao) == 0){
+                System.out.println("Servidor " + sFuncao + " está conectado, enviando ao cliente!");
                 return servico;
             }
         }
+        System.out.println("Não Foi possível encontrar nenhum serviço disponível do tipo " + sFuncao);
         return null;
     }
     
     @Override
-    public void conectarServico(ServidorIndividual servico){
+    public ServidorIndividual conectarServico(ServidorIndividual servico){
         if(servico instanceof ServidorIndividual){
-            _servicos.add(servico);
+            boolean tempStatus = true;
+            for(ServidorIndividual tempServ : _servicos){
+                if(tempServ.getName().compareTo(servico.getName()) == 0){
+                    if(tempServ.getAddress().compareTo(servico.getAddress()) == 0){
+                        servico.setStatus(1);
+                    } else {
+                        servico.setStatus(0);
+                    }
+                    tempStatus = false;
+                }
+            }
+            if(tempStatus == true){
+                _servicos.add(servico);
+                System.out.println(servico.getName() + " Conectado!");
+                servico.setStatus(2);
+            }
+            
         } else {
             throw new Error("Servidor esperava um objeto do tipo ServidorIndividual, mas recebeu tipo não aceito.");
         }
+        
+        return servico;
     }
     
     @Override
--- nameServer/src/BackEnd/SeveralServers/appAverage.java
+++ nameServer/src/BackEnd/SeveralServers/appAverage.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package BackEnd.SeveralServers;
-
+import BackEnd.Servidor;
+import Classes.ServidorIndividual;
 /**
  *
  * @author Engenharia de Computação - 9º Semestre
@@ -18,15 +19,42 @@ package BackEnd.SeveralServers;
 
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+import java.util.Scanner;
 
 public class appAverage {
     
     public static void main(String[] args) {
         try {
+            Scanner console = new Scanner(System.in);
+            String entrada = "";
+            
             functionServer avgServer = new averageServer();
             LocateRegistry.createRegistry(1100);
-            Naming.rebind("rmi://localhost:1100/Average", avgServer);
-            //Naming.lookup();  não instanciado
+            Naming.rebind("rmi://localhost:1100/Average", avgServer); 
+            
+            Servidor _server = (Servidor) Naming.lookup("rmi://localhost:1099/Servidor");
+            
+            ServidorIndividual _servico = new ServidorIndividual("Teste", "rmi://localhost:1100/Average");
+            
+            while(entrada.compareTo("Desligar") != 0){
+                entrada = console.next();
+                switch(entrada){
+                    case "Conectar":
+                        System.out.println(_server.conectarServico(_servico).getStatusMessage());
+                    break;
+                    case "Desconectar":
+                        if(_server.desconectarServico(_servico)){
+                            System.out.println("Desconectado do NameServer");
+                        }
+                    break;
+                    case "Desligar":
+                        System.exit(0);
+                    break;
+                }
+            }
+            
+            
+            
         } catch (Exception e) {
             System.out.println("Trouble: " + e.getMessage());
             System.exit(0);
