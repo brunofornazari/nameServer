948de198311864efb648fe7a871debc600d1b76d
lab653 <lab653@VO65327-080585.dominio.anhembimorumbi.edu.br>
2017-05-12 21:21:17 -0300

    Ainda não terminado

--- nameServer/src/BackEnd/Servers/expenseServerImpl.java
+++ nameServer/src/BackEnd/Servers/expenseServerImpl.java
@@ -37,6 +37,9 @@ public class expenseServerImpl extends UnicastRemoteObject implements expServer 
     public String ctrlDespesa(Usuario user, int mes, int ano) throws RemoteException {
         Scanner scan = new Scanner(System.in);
         String entrada = "";
+        String despesaDesc = "";
+        double despesaVal = 0;
+        Despesa despesa = new Despesa();
         List<Categoria> categorias = user.getCategorias();
         
         System.out.println("Digite o nome/descrição da despesa que você quer adicionar: ");
@@ -48,16 +51,45 @@ public class expenseServerImpl extends UnicastRemoteObject implements expServer 
             entrada = scan.next();
             switch(entrada) {
                 case "Adicionar":
-                    
+                    System.out.println("Digite o nome/descrição da despesa que você quer adicionar");
+                    despesaDesc = scan.next();
+                    System.out.println("Digite o valor da despesa que você quer adicionar");
+                    despesaVal = scan.nextDouble();
+                    for (Categoria categoria : categorias) {
+                        for (Periodo periodo : categoria.getPeriodos()) {
+                            if (periodo.getMes() == mes && periodo.getAno() == ano) {
+                                for (Despesa despesas : periodo.getDespesas()) {
+                                    if (despesas.getDescricao() == despesaDesc 
+                                            && despesas.getValor() == despesaVal) {
+                                        System.out.println("A despesa já existe");
+                                    }
+                                }
+                                //Adicionar a despesa
+                            }
+                        }
+                    }
                 break;
                 case "Remover":
-                    
+                    System.out.println("Digite o nome/descrição da despesa que você quer remover");
+                    despesaDesc = scan.next();
+                    for (Categoria categoria : categorias) {
+                        for (Periodo periodo : categoria.getPeriodos()) {
+                            if (periodo.getMes() == mes && periodo.getAno() == ano) {
+                                for (Despesa despesas : periodo.getDespesas()) {
+                                    if (despesas.getDescricao() == despesaDesc) {
+                                        
+                                    }
+                                }
+                            }
+                            System.out.println("A despesa não existe");
+                        }
+                    }
                 break;
                 case "Sair":
                 break;
             }
         }
-        return entrada;
+        return "Operação finalizada";
     }
     
 }
